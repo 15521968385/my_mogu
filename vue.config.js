a111d89fe41c7928595963de4c36955b1c82196b
谢敬涛 <15521968385@163.com>
2020-05-23 15:38:01 +0800

    项目初始化

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	devServer : {
+		open : true,
+		host : '192.168.1.5',
+		port : 8850,
+		hot : true,
+		disableHostCheck:true
+	},
+	lintOnSave : false,
+	configureWebpack: {
+        resolve: {
+            alias: { //配置别名,修改后需要重新编译才能生效
+                'src':'@',
+            }
+        }
+    }
+}
